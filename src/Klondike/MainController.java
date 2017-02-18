cf1cf8278f2f27813c7860bb1ff5149113db5130
Bureš <richardbures@email.cz>
2017-02-18 18:40:37 +0100

    Handlers

--- src/Klondike/MainController.java
+++ src/Klondike/MainController.java
@@ -1,7 +1,41 @@
 package Klondike;
 
+import javafx.event.ActionEvent;
+
 public class MainController {
     public void initialize() {
+
+    }
+
+    public void newGameHandler(ActionEvent event)
+    {
+
+    }
+
+
+    public void openGameHandler(ActionEvent event)
+    {
+
+    }
+    public void variantHandler(ActionEvent event)
+    {
+
+    }
+    public void statisticHandler(ActionEvent event)
+    {
+
+    }
+    public void exitHandler(ActionEvent event)
+    {
+
+    }
+
+    public void cardsHandler(ActionEvent event)
+    {
+
+    }
+    public void aboutHandler(ActionEvent event)
+    {
 
     }
 }
