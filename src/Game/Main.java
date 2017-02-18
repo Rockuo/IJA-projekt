e47d04b4988dc2dd258f1eff9eca9d5cf7c246f5
Marry <mariahalamova@gmail.com>
2017-02-18 15:34:36 +0100

    menu bar upravený

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("klondike.fxml"));
         primaryStage.setTitle("Klondike Solitaire");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
         Controller a = new Controller();
     }
