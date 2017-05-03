96ed76cbde12bfb380dfdf34ade7cdbfbf64df74
Bureš <bures@modpreneur.com>
2017-05-03 15:26:55 +0200

    fix

--- src/gui/klondike/Main.java
+++ src/gui/klondike/Main.java
@@ -21,7 +21,8 @@ public class Main extends Application {
         Parent root = fxmlLoader.load();
         ((MainController) fxmlLoader.getController()).setStage(primaryStage);
         primaryStage.setTitle("Solitaire");
-        primaryStage.setScene(new Scene(root, 1536, 1060));
+//        primaryStage.setScene(new Scene(root, 1536, 1060));
+        primaryStage.setScene(new Scene(root, 768, 530));
         primaryStage.show();
     }
 
